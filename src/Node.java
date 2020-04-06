a894214807096456f7a730bb7946cacb48511b7b
Gin641 <53481351+Gin641@users.noreply.github.com>
2020-04-06 16:08:43 +0700

    nah

---
+++ src/Node.java
@@ -0,0 +1,8 @@
+public class Node {
+    public int key;
+    public Node left;
+    public Node right;
+    public Node(int key){
+        this.key = key;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.root = new Node(10);
+        bst.root.left = new Node(20);
+        bst.root.right = new Node(30);
+        bst.root.left.left = new Node(40);
+        bst.root.right.right = new Node(50);
+        bst.postOrder();
+    }
+}
